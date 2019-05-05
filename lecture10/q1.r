556a51cfb02a4d504731d2f28e821504801cd944
Jack Xiaosong Xu <jackxxu@gmail.com>
2019-05-05 18:53:26 -0400

    lecture 10 changes

--- lecture10/q1.r
+++ lecture10/q1.r
@@ -1,8 +1,13 @@
-# part b: require sample size
-
 ua = 95
 u0 = 85
 var_p = 400
+
+# part a: D
+
+d <- (ua - u0)/sqrt(var_p)
+d #=> 0.5
+
+# part b: require sample size
 
 (sqrt(var_p) * (qnorm(0.025) + qnorm(0.1)) / (ua -u0)) ^ 2 #=> 42.02969
 
@@ -13,6 +18,6 @@ c_2_f <- function(c) { return (9.0/5 * c + 32) }
 ua_f = c_2_f(ua)
 u0_f = c_2_f(u0)
 var_p_f = (9.0/5)^2 * var_p
-(ua_f - u0_f)/sqrt(var_p_f)
+(ua_f - u0_f)/sqrt(var_p_f) #=> 0.5
 
 # part a:
--- lecture10/q2.r
+++ lecture10/q2.r
@@ -14,21 +14,24 @@ b_hat #=> 2.265498
 a_hat <- mean_y - b_hat * mean_x
 a_hat #=> -2.728216
 
-# part d: theta_hat
-theta_hat <- sse / (8-2)
-theta_hat  #=> 12.81539
+# part d: sigma_hat
+sigma_hat <- sse / (8-2)
+sigma_hat  #=> 12.81539
 
 # part e: standard error of b_hat
-se_b_hat <- sqrt(theta_hat/sxx)
+se_b_hat <- sqrt(sigma_hat/sxx)
 se_b_hat  #=> 0.5523842
 
 # part f: standard error of b_hat
 t <- b_hat/se_b_hat
-t #=> 23.20013
+t #=> 4.101309
 
 # part g: 
-u <- qt(0.975, 6)
+u <- qt(0.975, n-2)
 u #=> 2.446912
+
+p = (1 - pt(t, n-2))*2
+p #=> 0.006348021
 
 # part h
 r_sqrd <- (sxy*sxy)/sxx/syy
